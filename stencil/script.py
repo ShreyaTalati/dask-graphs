12b05ce881e45f63d43e9578d0133c9443d7cd25
dialecticDolt <will.ruys@gmail.com>
2024-01-16 20:47:04 -0500

    fix: fix dependencies and syntax on Dask graphs. added print logging for verification

--- stencil/script.py
+++ stencil/script.py
@@ -11,23 +11,85 @@ free_sleep = sleep_nogil
 lock_sleep = sleep_gil
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-workers', type=int, default=1, help='How many workers to use. This will perform a sample of 1 to workers by powers of 2')
-parser.add_argument('-width', type=int, default=0, help='The width of the task graph. If not set this is equal to nworkers.')
-parser.add_argument('-steps', type=int, default=4, help='The depth of the task graph.')
-parser.add_argument('-d', type=int, default=7, help='The size of the data if using numba busy kernel')
-parser.add_argument('-n', type=int, default=2**23, help='The size of the data if using numba busy kernel')
-parser.add_argument('-isync', type=int, default=0, help='Whether to synchronize (internally) using await at every timestep.')
-parser.add_argument('-vcus', type=int, default=1, help='Whether tasks use vcus to restrict how many can run on a single device')
-parser.add_argument('-deps', type=int, default=1, help='Whether tasks have dependencies on the prior iteration')
-parser.add_argument('-verbose', type=int, default=0, help='Verbose!')
+parser.add_argument(
+    "-workers",
+    type=int,
+    default=1,
+    help="How many workers to use. This will perform a sample of 1 to workers by powers of 2",
+)
+parser.add_argument(
+    "-width",
+    type=int,
+    default=0,
+    help="The width of the task graph. If not set this is equal to nworkers.",
+)
+parser.add_argument("-steps", type=int, default=4, help="The depth of the task graph.")
+parser.add_argument(
+    "-d", type=int, default=7, help="The size of the data if using numba busy kernel"
+)
+parser.add_argument(
+    "-n",
+    type=int,
+    default=2**23,
+    help="The size of the data if using numba busy kernel",
+)
+parser.add_argument(
+    "-isync",
+    type=int,
+    default=0,
+    help="Whether to synchronize (internally) using await at every timestep.",
+)
+parser.add_argument(
+    "-vcus",
+    type=int,
+    default=1,
+    help="Whether tasks use vcus to restrict how many can run on a single device",
+)
+parser.add_argument(
+    "-deps",
+    type=int,
+    default=1,
+    help="Whether tasks have dependencies on the prior iteration",
+)
+parser.add_argument("-verbose", type=int, default=0, help="Verbose!")
 
-parser.add_argument("-t", type=int, default=10, help='The task time in microseconds. These are hardcoded in this main.')
-parser.add_argument("-accesses", type=int, default=1, help='How many times the task stops busy waiting and accesses the GIL')
-parser.add_argument("-frac", type=float, default=0, help='The fraction of the total task time that the GIL is held')
+parser.add_argument(
+    "-t",
+    type=int,
+    default=10,
+    help="The task time in microseconds. These are hardcoded in this main.",
+)
+parser.add_argument(
+    "-accesses",
+    type=int,
+    default=1,
+    help="How many times the task stops busy waiting and accesses the GIL",
+)
+parser.add_argument(
+    "-frac",
+    type=float,
+    default=0,
+    help="The fraction of the total task time that the GIL is held",
+)
 
-parser.add_argument('-strong', type=int, default=0, help='Whether to use strong (1) or weak (0) scaling of the task time')
-parser.add_argument('-sleep', type=int, default=1, help='Whether to use the synthetic sleep (1) or the numba busy kernel (0)')
-parser.add_argument('-restrict', type=int, default=0, help='This does two separate things. If using isync it restricts to only waiting on the prior timestep. If using deps, it changes the dependencies from being a separate chain to depending on all tasks in the prior timestep')
+parser.add_argument(
+    "-strong",
+    type=int,
+    default=0,
+    help="Whether to use strong (1) or weak (0) scaling of the task time",
+)
+parser.add_argument(
+    "-sleep",
+    type=int,
+    default=1,
+    help="Whether to use the synthetic sleep (1) or the numba busy kernel (0)",
+)
+parser.add_argument(
+    "-restrict",
+    type=int,
+    default=0,
+    help="This does two separate things. If using isync it restricts to only waiting on the prior timestep. If using deps, it changes the dependencies from being a separate chain to depending on all tasks in the prior timestep",
+)
 
 args = parser.parse_args()
 
@@ -35,10 +97,11 @@ kernel_time = args.t / args.accesses
 free_time = kernel_time * (1 - args.frac)
 lock_time = kernel_time * (args.frac)
 
-def waste_time(t, i):
+
+def waste_time(id, deps):
     if args.verbose:
         inner_start_t = time.perf_counter()
-        print("Task", i, " | Start", flush=True)
+        print("Task", id, deps, " | Start", flush=True)
 
     for k in range(args.accesses):
         free_sleep(free_time)
@@ -46,35 +109,66 @@ def waste_time(t, i):
 
     if args.verbose:
         inner_end_t = time.perf_counter()
-        print("Task", i, " | Inner Time: ",
-              inner_end_t - inner_start_t, flush=True)
+        print(
+            "Task", id, deps, " | Inner Time: ", inner_end_t - inner_start_t, flush=True
+        )
 
+    return id
 
 
 def create_graph(steps, width):
     dsk = {}
     for t in range(steps):
         for i in range(width):
-            dependencies = [i, i - 1, i + 1]
-            dsk[(t, i, 'output')] = (waste_time, t, i)
-            dsk[(t, i, 'dependencies')] = [(t, i_dep, 'output') for i_dep in dependencies
-                                           if 0 <= i_dep < width]
+            dependency_idx = [i, i - 1, i + 1]
+
+            if t > 0:
+                dependencies = [
+                    f"T_{t-1}_{j}" for j in dependency_idx if 0 <= j < width
+                ]
+            else:
+                dependencies = []
+
+            id = (t, i)
+            sid = f"T_{t}_{i}"
+
+            dsk[sid] = (waste_time, id, dependencies)
 
     return dsk
 
-if __name__ == '__main__':
 
-    print(', '.join([str('workers'), str('n'), str('task_time'), str(
-            'accesses'), str('frac'), str('total_time')]), flush=True)
+if __name__ == "__main__":
+    print(
+        ", ".join(
+            [
+                str("workers"),
+                str("n"),
+                str("task_time"),
+                str("accesses"),
+                str("frac"),
+                str("total_time"),
+            ]
+        ),
+        flush=True,
+    )
 
     with dask.config.set(pool=ThreadPoolExecutor(args.workers)):
         start_t = time.perf_counter()
         dsk = create_graph(args.steps, args.width)
-        result = get(dsk, [(args.steps - 1, args.width - 1, 'output')])
-        # dask_graph.get([(t, i, 'output') for t in range(num_tasks) for i in range(num_tasks)])
-        # result = get(dsk, [(t, i, 'output') for t in range(args.steps) for i in range(args.steps)])
+        result = get(dsk, [f"T_{args.steps-1}_{k}" for k in range(args.width)])
         end_t = time.perf_counter()
         elapsed_t = end_t - start_t
-        # dask.visualize(dsk, filename='dask_graph_stencil', format='png', engine="cytoscape")
-        print(', '.join([str(args.workers), str(args.steps), str(args.t),
-              str(args.accesses), str(args.frac), str(elapsed_t)]), flush=True)
+
+        print(
+            ", ".join(
+                [
+                    str(args.workers),
+                    str(args.steps),
+                    str(args.t),
+                    str(args.accesses),
+                    str(args.frac),
+                    str(elapsed_t),
+                ]
+            ),
+            flush=True,
+        )
--- tree_dask.py
+++ tree_dask.py
@@ -11,23 +11,85 @@ free_sleep = sleep_nogil
 lock_sleep = sleep_gil
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-workers', type=int, default=1, help='How many workers to use. This will perform a sample of 1 to workers by powers of 2')
-parser.add_argument('-width', type=int, default=0, help='The width of the task graph. If not set this is equal to nworkers.')
-parser.add_argument('-steps', type=int, default=4, help='The depth of the task graph.')
-parser.add_argument('-d', type=int, default=7, help='The size of the data if using numba busy kernel')
-parser.add_argument('-n', type=int, default=2**23, help='The size of the data if using numba busy kernel')
-parser.add_argument('-isync', type=int, default=0, help='Whether to synchronize (internally) using await at every timestep.')
-parser.add_argument('-vcus', type=int, default=1, help='Whether tasks use vcus to restrict how many can run on a single device')
-parser.add_argument('-deps', type=int, default=1, help='Whether tasks have dependencies on the prior iteration')
-parser.add_argument('-verbose', type=int, default=0, help='Verbose!')
+parser.add_argument(
+    "-workers",
+    type=int,
+    default=1,
+    help="How many workers to use. This will perform a sample of 1 to workers by powers of 2",
+)
+parser.add_argument(
+    "-width",
+    type=int,
+    default=0,
+    help="The width of the task graph. If not set this is equal to nworkers.",
+)
+parser.add_argument("-steps", type=int, default=4, help="The depth of the task graph.")
+parser.add_argument(
+    "-d", type=int, default=7, help="The size of the data if using numba busy kernel"
+)
+parser.add_argument(
+    "-n",
+    type=int,
+    default=2**23,
+    help="The size of the data if using numba busy kernel",
+)
+parser.add_argument(
+    "-isync",
+    type=int,
+    default=0,
+    help="Whether to synchronize (internally) using await at every timestep.",
+)
+parser.add_argument(
+    "-vcus",
+    type=int,
+    default=1,
+    help="Whether tasks use vcus to restrict how many can run on a single device",
+)
+parser.add_argument(
+    "-deps",
+    type=int,
+    default=1,
+    help="Whether tasks have dependencies on the prior iteration",
+)
+parser.add_argument("-verbose", type=int, default=0, help="Verbose!")
 
-parser.add_argument("-t", type=int, default=10, help='The task time in microseconds. These are hardcoded in this main.')
-parser.add_argument("-accesses", type=int, default=1, help='How many times the task stops busy waiting and accesses the GIL')
-parser.add_argument("-frac", type=float, default=0, help='The fraction of the total task time that the GIL is held')
+parser.add_argument(
+    "-t",
+    type=int,
+    default=10,
+    help="The task time in microseconds. These are hardcoded in this main.",
+)
+parser.add_argument(
+    "-accesses",
+    type=int,
+    default=1,
+    help="How many times the task stops busy waiting and accesses the GIL",
+)
+parser.add_argument(
+    "-frac",
+    type=float,
+    default=0,
+    help="The fraction of the total task time that the GIL is held",
+)
 
-parser.add_argument('-strong', type=int, default=0, help='Whether to use strong (1) or weak (0) scaling of the task time')
-parser.add_argument('-sleep', type=int, default=1, help='Whether to use the synthetic sleep (1) or the numba busy kernel (0)')
-parser.add_argument('-restrict', type=int, default=0, help='This does two separate things. If using isync it restricts to only waiting on the prior timestep. If using deps, it changes the dependencies from being a separate chain to depending on all tasks in the prior timestep')
+parser.add_argument(
+    "-strong",
+    type=int,
+    default=0,
+    help="Whether to use strong (1) or weak (0) scaling of the task time",
+)
+parser.add_argument(
+    "-sleep",
+    type=int,
+    default=1,
+    help="Whether to use the synthetic sleep (1) or the numba busy kernel (0)",
+)
+parser.add_argument(
+    "-restrict",
+    type=int,
+    default=0,
+    help="This does two separate things. If using isync it restricts to only waiting on the prior timestep. If using deps, it changes the dependencies from being a separate chain to depending on all tasks in the prior timestep",
+)
 
 args = parser.parse_args()
 
@@ -35,10 +97,11 @@ kernel_time = args.t / args.accesses
 free_time = kernel_time * (1 - args.frac)
 lock_time = kernel_time * (args.frac)
 
-def waste_time(t, i):
+
+def waste_time(tid, deps):
     if args.verbose:
         inner_start_t = time.perf_counter()
-        print("Task", i, " | Start", flush=True)
+        print("Task", tid, deps, " | Start", flush=True)
 
     for k in range(args.accesses):
         free_sleep(free_time)
@@ -46,8 +109,16 @@ def waste_time(t, i):
 
     if args.verbose:
         inner_end_t = time.perf_counter()
-        print("Task", i, " | Inner Time: ",
-              inner_end_t - inner_start_t, flush=True)
+        print(
+            "Task",
+            tid,
+            deps,
+            " | Inner Time: ",
+            inner_end_t - inner_start_t,
+            flush=True,
+        )
+
+    return tid
 
 
 def create_graph(num_levels):
@@ -56,48 +127,71 @@ def create_graph(num_levels):
     # Scatter phase
     for level in range(num_levels + 1):
         tasks_in_level = 2 ** (level)
-        for i in range(tasks_in_level):
-            # task_idx = (2 * num_levels - level, i)
+
+        for j in range(tasks_in_level):
+            task_idx = (2 * num_levels - level, j)
+            sid = f"T_{task_idx[0]}_{task_idx[1]}"
+
             dependencies = []
             if level > 0:
-                dependency = (2 * num_levels - level + 1, i // 2)
-                dependencies.append(dependency)
+                parent = (2 * num_levels - level + 1, j // 2)
+                sparent = f"T_{parent[0]}_{parent[1]}"
+                dependencies.append(sparent)
 
-            dsk[(2 * num_levels - level, i, 'output')] = (waste_time, 2 * num_levels - level, i)
-            dsk[(2 * num_levels - level, i, 'dependencies')] = [(i_dep[0], i_dep[1], 'output') for i_dep in dependencies]
-            
+            dsk[sid] = (waste_time, task_idx, dependencies)
+
     # Reduction phase
     for level in range(num_levels - 1, -1, -1):
         tasks_in_level = 2 ** (level)
         print("tasks in level", tasks_in_level)
 
-        for i in range(tasks_in_level):
-            # task_idx = (level, i)
-            
+        for j in range(tasks_in_level):
+            task_idx = (level, j)
+            sid = f"T_{task_idx[0]}_{task_idx[1]}"
+
             # Task Dependencies
             dependencies = []
             for k in range(2):
-                dependency = (level + 1, 2 * i + k)
-                dependencies.append(dependency)
+                dependency = (level + 1, 2 * j + k)
+                sdependency = f"T_{dependency[0]}_{dependency[1]}"
+                dependencies.append(sdependency)
 
-            dsk[(level, i, 'output')] = (waste_time, level, i)
-            dsk[(level, i, 'dependencies')] = [(i_dep[0], i_dep[1], 'output') for i_dep in dependencies]
+            dsk[sid] = (waste_time, task_idx, dependencies)
 
     return dsk
 
 
-if __name__ == '__main__':
-
-    print(', '.join([str('workers'), str('n'), str('task_time'), str(
-            'accesses'), str('frac'), str('total_time')]), flush=True)
+if __name__ == "__main__":
+    print(
+        ", ".join(
+            [
+                str("workers"),
+                str("n"),
+                str("task_time"),
+                str("accesses"),
+                str("frac"),
+                str("total_time"),
+            ]
+        ),
+        flush=True,
+    )
 
     with dask.config.set(pool=ThreadPoolExecutor(args.workers)):
         start_t = time.perf_counter()
         dsk = create_graph(args.steps)
-        # dask_graph.get([(t, i, 'output') for t in range(num_tasks) for i in range(num_tasks)])
-        result = get(dsk, [(0, 0, 'output')])
+        result = get(dsk, ["T_0_0"])
         end_t = time.perf_counter()
         elapsed_t = end_t - start_t
-        # dask.visualize(dsk, filename='dask_graph_stencil', format='png', engine="cytoscape")
-        print(', '.join([str(args.workers), str(args.steps), str(args.t),
-              str(args.accesses), str(args.frac), str(elapsed_t)]), flush=True)
+        print(
+            ", ".join(
+                [
+                    str(args.workers),
+                    str(args.steps),
+                    str(args.t),
+                    str(args.accesses),
+                    str(args.frac),
+                    str(elapsed_t),
+                ]
+            ),
+            flush=True,
+        )
